f7bf944d171f4f26a9876ecb9af37bb551da438d
Crowdbotics <team@crowdbotics.com>
2024-03-17 05:43:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "moneyguard-v2-47109",
+  url: "https://moneyguard-v2-47109.botics.co",
+  api: "https://moneyguard-v2-47109.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
